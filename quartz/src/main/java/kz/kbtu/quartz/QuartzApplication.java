5861339d08825d7de52d57e2fe180c9dc7668af2
szarina <zarina.saden@gmail.com>
2024-04-20 10:01:39 +0500

    Initial commit

---
+++ quartz/src/main/java/kz/kbtu/quartz/QuartzApplication.java
@@ -0,0 +1,13 @@
+package kz.kbtu.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzApplication.class, args);
+	}
+
+}
---
+++ quartz/src/test/java/kz/kbtu/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kbtu.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
