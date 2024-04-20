81a81eedabb827394094ae0e3747f9ee5eaa0902
Toktamys Gulim <glimtoktamys82@gmail.com>
2024-04-20 15:39:47 +0600

    Initial commit

--- quartz/src/main/java/kz/kbtu/quartz/QuartzApplication.java
+++ quartz/src/main/java/kz/kbtu/quartz/QuartzApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuartzApplication {
+public class
+QuartzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzApplication.class, args);
